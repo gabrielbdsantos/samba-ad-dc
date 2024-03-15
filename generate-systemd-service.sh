66534e048e0abaf0f6e3099b5e1a34ac270fcbc9
Gabriel B. Santos <9348147+gabrielbdsantos@users.noreply.github.com>
2024-03-14 21:27:49 -0300

    chore: update the systemd unit

--- generate-systemd-service.sh
+++ generate-systemd-service.sh
@@ -5,13 +5,18 @@ set -e
 HERE=$(realpath -- $(dirname ${BASH_SOURCE[0]}))
 PODMAN=$(which podman-compose)
 
-cat >"$1" <<EOF
+usage() {
+	echo "USAGE: ${BASH_SOURCE[0]} UNIT_NAME"
+}
+
+create-systemd-unit() {
+	cat >"$1" <<EOF
 [Unit]
 Description=Containerized Samba AD DC
 After=network.target
 
 [Service]
-Restart=always
+Type=forking
 WorkingDirectory=$HERE
 ExecStart=$PODMAN up -d
 ExecStop=$PODMAN down -v
@@ -19,3 +24,10 @@ ExecStop=$PODMAN down -v
 [Install]
 WantedBy=multi-user.target
 EOF
+}
+
+if [[ -n "$1" ]]; then
+	create-systemd-unit "$1"
+else
+	usage
+fi
